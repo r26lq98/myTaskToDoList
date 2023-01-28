6a8de53d09db0978012c545333009207aefb5030
r26lq98 <r26lq98@gmail.com>
2023-01-28 17:58:43 +0700

    Update menu edit 00

--- js/tabel.js
+++ js/tabel.js
@@ -1,14 +1,16 @@
 // Tabel_Task_Root
 const _task = document.querySelector('table._task'),
+thead = _task.querySelector('tr._thead'),
 menuEdit = document.createElement('div'),
 [..._tbody] = _task.querySelectorAll('._tbody');
 menuEdit.classList.add('tmenu_edit');
 menuEdit.innerHTML = 
   `<ul id="tmenu_edit">
-      <li><a href="">item1</a></li>
-      <li><a href="">item2</a></li>
-      <li><a href="">item3</a></li>
+      <li><a href="">Tugas Baru</a></li>
+      <li><a href="">Detail</a></li>
+      <li><a href="">Hapus</a></li>
   </ul>`;
+let  [_edit, _number, _icon, _taskName, _alarmSet] = thead.children;
 _tbody.forEach(item => {
   let [edit, number, icon, taskName, ...alarmSet] = item.cells;
   //Cells add id
@@ -17,19 +19,19 @@ _tbody.forEach(item => {
   icon.id = "icon";
   taskName.id = "taskName";
   //Cells add Attributes
-  edit.setAttribute("data-title", "Edit");
-  number.setAttribute("data-title", "Id");
-  icon.setAttribute("data-title", "icon");
-  taskName.setAttribute("data-title", "Task Name");
+  edit.setAttribute("data-title", _edit.textContent);
+  number.setAttribute("data-title", _number.textContent);
+  icon.setAttribute("data-title", _icon.textContent);
+  taskName.setAttribute("data-title", _taskName.textContent);
   alarmSet.map(data => {
     data.id = "alarm-set";
-    data.setAttribute("data-title", "Alarm Set")
+    data.setAttribute("data-title", _alarmSet.textContent)
   });
   //Menu Edit Root
   let [iconEdit] = edit.children;
   iconEdit.addEventListener("click", function(){
     menuEdit.style.display = "block";
-    this.offsetParent.appendChild(menuEdit)
+    this.offsetParent.appendChild(menuEdit);
     this.nextElementSibling.children.tmenu_edit.style.top = this.offsetParent.offsetTop + "px";
   });
   menuEdit.addEventListener('click', function(x){
