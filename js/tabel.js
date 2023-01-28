af75ffe4b618c4a3eb6ebb8f352f49458e8c08b8
r26lq98 <r26lq98@gmail.com>
2023-01-28 15:52:50 +0700

    👉 Update menu edit

--- js/tabel.js
+++ js/tabel.js
@@ -1,6 +1,14 @@
 // Tabel_Task_Root
 const _task = document.querySelector('table._task'),
+menuEdit = document.createElement('div'),
 [..._tbody] = _task.querySelectorAll('._tbody');
+menuEdit.classList.add('tmenu_edit');
+menuEdit.innerHTML = 
+  `<ul id="tmenu_edit">
+      <li><a href="">item1</a></li>
+      <li><a href="">item2</a></li>
+      <li><a href="">item3</a></li>
+  </ul>`;
 _tbody.forEach(item => {
   let [edit, number, icon, taskName, ...alarmSet] = item.cells;
   //Cells add id
@@ -18,9 +26,10 @@ _tbody.forEach(item => {
     data.setAttribute("data-title", "Alarm Set")
   });
   //Menu Edit Root
-  let [iconEdit, menuEdit] = edit.children;
+  let [iconEdit] = edit.children;
   iconEdit.addEventListener("click", function(){
-    this.nextElementSibling.style.display = "block";
+    menuEdit.style.display = "block";
+    this.offsetParent.appendChild(menuEdit)
     this.nextElementSibling.children.tmenu_edit.style.top = this.offsetParent.offsetTop + "px";
   });
   menuEdit.addEventListener('click', function(x){
