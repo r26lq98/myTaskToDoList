60adbfba8cf22d025b67064331da03f6b4d1a175
r26lq98 <r26lq98@gmail.com>
2023-01-28 18:13:36 +0700

    👉 Last Update focus_menu_edit_00

--- js/tabel.js
+++ js/tabel.js
@@ -1,41 +1,43 @@
-// Tabel_Task_Root
+  // Tabel_Task_Root
 const _task = document.querySelector('table._task'),
-thead = _task.querySelector('tr._thead'),
-menuEdit = document.createElement('div'),
-[..._tbody] = _task.querySelectorAll('._tbody');
-menuEdit.classList.add('tmenu_edit');
-menuEdit.innerHTML = 
+  thead = _task.querySelector('tr._thead'),
+  menuEdit = document.createElement('div'),
+  [..._tbody] = _task.querySelectorAll('._tbody');
+  menuEdit.classList.add('tmenu_edit');
+  menuEdit.innerHTML = 
   `<ul id="tmenu_edit">
-      <li><a href="">Tugas Baru</a></li>
-      <li><a href="">Detail</a></li>
-      <li><a href="">Hapus</a></li>
+  <li><a href="">Tugas Baru</a></li>
+  <li><a href="">Detail</a></li>
+  <li><a href="">Hapus</a></li>
   </ul>`;
-let  [_edit, _number, _icon, _taskName, _alarmSet] = thead.children;
+  let  [_edit, _number, _icon, _taskName, _alarmSet] = thead.children;
 _tbody.forEach(item => {
-  let [edit, number, icon, taskName, ...alarmSet] = item.cells;
-  //Cells add id
-  edit.id = "edit";
-  number.id = "number";
-  icon.id = "icon";
-  taskName.id = "taskName";
-  //Cells add Attributes
-  edit.setAttribute("data-title", _edit.textContent);
-  number.setAttribute("data-title", _number.textContent);
-  icon.setAttribute("data-title", _icon.textContent);
-  taskName.setAttribute("data-title", _taskName.textContent);
-  alarmSet.map(data => {
-    data.id = "alarm-set";
-    data.setAttribute("data-title", _alarmSet.textContent)
+    let edits = item.insertCell(0),
+    [edit, number, icon, taskName, ...alarmSet] = item.cells;
+    edits.innerHTML = `<a href="#tmenu_edit" class="_tmenu_edit"><i class="fas fa-ellipsis-h"></i></a>`;
+    //Cells add id
+    edit.id = "edit";
+    number.id = "number";
+    icon.id = "icon";
+    taskName.id = "taskName";
+    //Cells add Attributes
+    edit.setAttribute("data-title", _edit.textContent);
+    number.setAttribute("data-title", _number.textContent);
+    icon.setAttribute("data-title", _icon.textContent);
+    taskName.setAttribute("data-title", _taskName.textContent);
+    alarmSet.map(data => {
+      data.id = "alarm-set";
+      data.setAttribute("data-title", _alarmSet.textContent)
+    });
+    //Menu Edit Root
+    let [iconEdit] = edit.children;
+    iconEdit.addEventListener("click", function(){
+      menuEdit.style.display = "block";
+      this.offsetParent.appendChild(menuEdit);
+      this.nextElementSibling.children.tmenu_edit.style.top = this.offsetParent.offsetTop + "px";
+    });
+    menuEdit.addEventListener('click', function(x){
+      x.target === this ? this.style.display = "none" : this.style.display = "block";
+    });
+    window.innerWidth < 540 ? menuEdit.click() : false; 
   });
-  //Menu Edit Root
-  let [iconEdit] = edit.children;
-  iconEdit.addEventListener("click", function(){
-    menuEdit.style.display = "block";
-    this.offsetParent.appendChild(menuEdit);
-    this.nextElementSibling.children.tmenu_edit.style.top = this.offsetParent.offsetTop + "px";
-  });
-  menuEdit.addEventListener('click', function(x){
-    x.target === this ? this.style.display = "none" : this.style.display = "block";
-  });
-  window.innerWidth < 540 ? menuEdit.click() : false; 
-});
