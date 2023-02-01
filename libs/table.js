f7a4f2cd1df32ff68a1ed6c2525a485db0e92e75
r26lq98 <r26lq98@gmail.com>
2023-02-01 08:59:39 +0700

    Fix bugg 6 👉 11_ps

--- libs/table.js
+++ libs/table.js
@@ -1,18 +1,49 @@
 export default function(){
   // Tabel_Task_Root
   const _task = document.querySelector('table._task'),
-  thead = _task.querySelector('tr._thead'),
-  tx = document.getElementById('tfbody'),
+  thead = _task.insertRow(0),
+  tfbody = document.getElementById('tfbody'),
   menuEdit = document.createElement('div'),
+  // thead = document.createElement('tr');
   newTask = document.getElementById('newTask'),
   [..._tbody] = _task.querySelectorAll('._tbody');
+  
+  
+  thead.classList.add('_thead');
+  thead.innerHTML = `
+        <th>edit</th>
+        <th>Id</th>
+        <th>icon</th>
+        <th class="taskName">Nama Tugas</th>
+        <th id="alarm-set">Alarm Set 1</th>
+        <th id="alarm-desc">Deskripsi 1</th>
+        <th id="alarm-set">Alarm Set 2</th>
+        <th id="alarm-desc">Deskripsi 2</th>
+        <th id="alarm-set">Alarm Set 3</th>
+        <th id="alarm-desc">Deskripsi 3</th>
+        <th id="alarm-set">Alarm Set 4</th>
+        <th id="alarm-desc">Deskripsi 4</th>
+        <th id="alarm-set">Alarm Set 5</th>
+        <th id="alarm-desc">Deskripsi 5</th>
+        <th id="alarm-set">Alarm Set 6</th>
+        <th id="alarm-desc">Deskripsi 6</th>
+        <th id="alarm-set">Alarm Set 7</th>
+        <th id="alarm-desc">Deskripsi 7</th>
+        <th id="alarm-set">Alarm Set 8</th>
+        <th id="alarm-desc">Deskripsi 8</th>
+        <th id="alarm-set">Alarm Set 9</th>
+        <th id="alarm-desc">Deskripsi 9</th>
+        <th id="alarm-set">Alarm Set 10</th>
+        <th id="alarm-desc">Deskripsi 10</th>
+  `;
+  
   menuEdit.classList.add('tmenu_edit');
   menuEdit.innerHTML = 
   `<ul id="tmenu_edit" class="tmenu_edit">
   <li><a href="#newTask" id="newTask"><i class="fas fa-plus"></i>Tugas Baru</a></li>
   <li><a href="#_edit" id="_edit"><i class="fas fa-pen"></i>Edit</a></li>
-  <li><a href=""><i class="fas fa-info-circle"></i>Detail</a></li>
-  <li><a href=""><i class="fas fa-trash-alt"></i>Hapus</a></li>
+  <li><a href="#"><i class="fas fa-info-circle"></i>Detail</a></li>
+  <li><a href="#"><i class="fas fa-trash-alt"></i>Hapus</a></li>
   </ul>`;
   
   let  [_edit, _number, _icon, _taskName, ..._alarms] = thead.children;
@@ -27,14 +58,15 @@ export default function(){
     iconEdit.addEventListener("click", function(){
       this.offsetParent.appendChild(menuEdit);
       const tabMenu = this.nextElementSibling.children.tmenu_edit,
-      [newTask, details, removeTask] = Array.from(tabMenu.children);
-      
+      [newTask, edetTb, details, removeTask] = Array.from(tabMenu.children);
+      edetTb.hidden = true;
+      details.hidden = true;
+      removeTask.hidden = true;
       tabMenu.style.top = this.offsetParent.offsetTop + "px";
       tabMenu.style.left = this.offsetLeft + "px";
       //New Task
       newTask.addEventListener('click', function(){
         
-        // console.log(iconEdit.offsetParent.parentElement)
         const tr = _task.insertRow(_task.rows.length),
         $edit = tr.insertCell(0),
         $id = tr.insertCell(1),
@@ -114,7 +146,10 @@ export default function(){
         alarmDesc8.textContent = "Contoh deskripsi 8";
         alarmDesc9.textContent = "Contoh deskripsi 9";
         alarmDesc10.textContent = "Contoh deskripsi 10";
-        tx.remove();
+        tfbody.remove();
+        edetTb.hidden = false;
+        details.hidden = false;
+        removeTask.hidden = false;
         $id.textContent = (tr.rowIndex).toString().padStart(2, '0');
       });
     });
